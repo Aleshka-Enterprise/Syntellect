1ebd20708b943aaf9501af469ecfe1549a88f1ca
Aleshka-Enterprise <Sleska4@yandex.ru>
2024-01-25 15:14:59 +0300

    Решение

---
+++ tasks.js
@@ -0,0 +1,22 @@
+// Задачка 1
+const climbingDay = (distance, passesPerDay, compensate) => {
+  return Math.ceil((distance - passesPerDay) / (passesPerDay - compensate)) + 1;
+}
+
+console.log(`Черепашка залезет на холм ${climbingDay(100, 50, 30)} сутки.`)
+
+
+// Задачка 2
+function getCountingHandshakes(numberOfPeople) {
+  return numberOfPeople * (numberOfPeople - 1) / 2
+}
+
+console.log(`Общее количество рукопожатий для 10 человек: ${getCountingHandshakes(10)}.`);
+
+
+// Задачка 3
+const uniqueWords = (list) => {
+  return list.split(",").filter((word, index, arr) => word && arr.indexOf(word) ===  index)
+} 
+
+console.log(uniqueWords("кошка,собака,лош6адь,корова,кошка,пёс,кошка,слон,"))
